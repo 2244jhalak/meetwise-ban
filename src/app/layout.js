8123002be15486119afa736caabfc26743bff090
Jhalak <stdjhalak@gmail.com>
2024-09-17 23:41:44 +0600

    clear problem

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Ready from "./components/Homepage/Ready";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -23,7 +23,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <Ready></Ready>   
+      {children}
       </body>
     </html>
   );
