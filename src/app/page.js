457574e22600e3a2de8a6d5ef4169bb07171da44
oishee <oisheekhan90@gmail.com>
2024-09-18 18:47:18 +0600

    HowItWorks Section done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 
+import Banner from "./components/Homepage/Banner";
 import HowItWork from "./components/Homepage/HowItWork";
 
 export default function Home() {
   return (
   <div>
+    <Banner></Banner>
     <HowItWork></HowItWork>
   </div>
   );
